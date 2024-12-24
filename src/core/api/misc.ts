3fb79deff717fdf610ad88ebf27063d0a1222ce0
Rekai Musuka <rekai@musuka.dev>
2024-12-23 23:13:42 -0600

    chore: remove axios

--- src/core/api/misc.ts
+++ src/core/api/misc.ts
@@ -1,17 +1,14 @@
 import { ApiResponse } from "@/types/structs";
-import axiosInstance from "../axios";
-import { Endpoints } from "../endpoints";
 import { GoogleAutoCompleteDto } from "@/types/dtos";
+import { Endpoints } from "../endpoints";
+import firebaseApp from "@/firebase/config";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const functions = getFunctions(firebaseApp);
+const getGooglePlaceAutoComplete = httpsCallable(functions, Endpoints.GET_GOOGLE_PLACE_AUTO_COMPLETE);
 
 export const googleAutoComplete = async (payload: GoogleAutoCompleteDto): Promise<ApiResponse<any>> => {
-      try {
-        const res = await axiosInstance.post<ApiResponse<any>>(Endpoints.GET_GOOGLE_PLACE_AUTO_COMPLETE, {
-          data: {
-            ...payload
-          }
-        });
-        return res.data;
-      } catch (err) {
-        throw err;
-      }
-    }; 
+  const res = await getGooglePlaceAutoComplete({ data: payload });
+  return res.data as any;
+};
+
--- src/core/api/quote.ts
+++ src/core/api/quote.ts
@@ -1,36 +1,22 @@
 import { BookMoveDto } from "@/types/dtos";
-import axiosInstance from "../axios";
 import { Endpoints } from "../endpoints";
 import { ApiResponse, Quote } from "@/types/structs";
+import firebaseApp from "@/firebase/config";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+
+const functions = getFunctions(firebaseApp);
+const _getQuote = httpsCallable(functions, Endpoints.GET_QUOTE);
+const _getQuotes = httpsCallable(functions, Endpoints.GET_QUOTES);
+
 
 export const getQuote = async (payload: Partial<BookMoveDto>): Promise<any> => {
-  try {
-    const res = await axiosInstance.post(Endpoints.GET_QUOTE, {
-      data: {
-        searchRequest: JSON.stringify(payload),
-      },
-    });
-    return res;
-  } catch (err) {
-    throw err;
-  }
+  const res = await _getQuote({ data: { searchRequest: JSON.stringify(payload) } });
+  return res.data as any;
 };
 
-export const getQuotes = async (
-  payload: Partial<BookMoveDto>
-): Promise<ApiResponse<Array<Quote>>> => {
-  try {
-    const res = await axiosInstance.post<ApiResponse<Array<Quote>>>(
-      Endpoints.GET_QUOTES,
-      {
-        data: {
-          searchRequest: JSON.stringify(payload),
-        },
-      }
-    );
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
+export const getQuotes = async (payload: Partial<BookMoveDto>): Promise<ApiResponse<Array<Quote>>> => {
+  const res = await _getQuotes({ data: { searchRequest: JSON.stringify(payload) } });
+  return res.data as ApiResponse<Array<Quote>>;
 };
 
