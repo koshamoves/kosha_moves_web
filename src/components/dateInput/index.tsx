b2ab09b65621aa2e692555ca64bae163b3d4972d
Rekai Musuka <rekai@musuka.dev>
2025-05-09 17:38:45 -0500

    fix: complete 2025-QA-KOSH-0001
    dismiss the DateInput popover on selection of valid date

--- src/components/dateInput/index.tsx
+++ src/components/dateInput/index.tsx
@@ -1,12 +1,21 @@
 import { Calendar } from "../calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "../popover"
-import { FC, ReactNode } from "react"
+import { FC, ReactNode, MouseEvent, useRef } from "react"
+import { ActiveModifiers } from "react-day-picker"
+import { Close } from "@radix-ui/react-popover";
 
 interface Props {
       field?: any;
       trigger: ReactNode;
 }
 export const DateInput: FC<Props> = ({ field, trigger }) => {
+      const closeRef = useRef<HTMLButtonElement | null>(null);
+
+      const handleSelect = (day: Date | undefined, selectedDay: Date, modifiers: ActiveModifiers, e: MouseEvent) => {
+            field.onChange(day, selectedDay, modifiers, e);
+            closeRef.current?.click();
+      };
+
       return (
             <Popover>
                   <PopoverTrigger asChild>
@@ -16,11 +25,12 @@ export const DateInput: FC<Props> = ({ field, trigger }) => {
                         <Calendar
                               mode="single"
                               selected={field.value}
-                              onSelect={field.onChange}
+                              onSelect={handleSelect}
                               disabled={(date) => date < new Date("1900-01-01")
                               }
                               initialFocus
                         />
+                        <Close className="hidden" ref={closeRef} />
                   </PopoverContent>
             </Popover>
       )
